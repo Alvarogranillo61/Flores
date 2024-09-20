1f0627d25c6c74b9bd5b71458a1757416e1bad5f
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-09-19 18:07:56 -0600

    Publish

---
+++ sunflower.js
@@ -0,0 +1,26 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+var lyricsData = [
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    var fadeInDuration = 0.1;
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 500);
